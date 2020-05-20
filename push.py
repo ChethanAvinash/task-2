e00e325ecb3432b0842bd548c6b9e9b794336c2e
ChethanAvinash <chethan.avinash45@gmail.com>
2020-05-20 00:03:04 -0700

    changed

--- push.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.system("git add .")
-os.system("git commit -m 'changed'")
-os.system("git remote set-url origin https://ChethanAvinash:Chethan2606@github.com/ChethanAvinash/task-2.git")
-os.system("git push")
